2d3e02eaa671889b9f430152243af9e2fe16efda
jose999888 <jose999888@users.noreply.github.com>
2025-10-31 14:53:05 +0000

    deploy: jose999888/my-resume-private@7ec4a314acce4aed9de9f4c4e3a954130a754e0c

--- static-js/touch.js
+++ static-js/touch.js
@@ -1,17 +1,23 @@
-// 阻止双指放大
-document.addEventListener('gesturestart', (event) => {
-  event.preventDefault()
-}, false)
+document.addEventListener('gesturestart', (e) => {
+  e.preventDefault() // iOS Safari
+}, { passive: false })
 
-// 阻止双击放大
-document.addEventListener('touchstart', (event) => {
-  event.preventDefault()
-},{passive:false})
 let lastTouchEnd = 0
-document.addEventListener('touchend', (event) => {
-  const now = (new Date()).getTime()
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault()
+
+document.addEventListener('touchstart', (e) => {
+  // 只有多指触控才阻止
+  if (e.touches.length > 1) {
+    e.preventDefault()
   }
+}, { passive: false })
+
+document.addEventListener('touchend', (e) => {
+  const now = Date.now()
+
+  // 双击缩放
+  if (e.touches.length === 0 && (now - lastTouchEnd <= 300)) {
+    e.preventDefault()
+  }
+
   lastTouchEnd = now
-}, false)
+}, { passive: false })
